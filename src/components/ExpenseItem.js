e96acd2be6a40bb47939e5b2522247b18918cf00
ShrutiDeore76 <shrutideore76@gmail.com>
2024-03-10 23:47:58 +0530

    Initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,23 @@
+function ExpenseItem() {
+    return (
+      <div>
+        <h2>Expense Items</h2>
+        <table>
+            <tr>
+                <td>Food</td>
+                <td>Rs.10</td>
+            </tr>
+            <tr>
+                <td>Petrol</td>
+                <td>Rs.100</td>
+            </tr>
+            <tr>
+                <td>Movies</td>
+                <td>Rs.200</td>
+            </tr>
+        </table>
+      </div>
+    );
+  }
+  
+  export default ExpenseItem;
