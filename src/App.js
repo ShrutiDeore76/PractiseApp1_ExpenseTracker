4026b405642aba44d4ae5cc9c9003c94857b2832
ShrutiDeore76 <shrutideore76@gmail.com>
2024-03-11 16:04:20 +0530

    Adding props and rendering data - practise Adding Updated files App.js and ExpenseItem.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses  =[
+    {
+      id: 'e1',
+      title: 'Food',
+      amount: 'Rs.10',
+      LocationOfExpenditure : "Nashik",
+    },
+    {
+      id: 'e2',
+      title: 'Petrol',
+      amount: 'Rs.100',
+      LocationOfExpenditure : "Nashik",
+    },
+    {
+      id: 'e3',
+      title: 'Movies',
+      amount: 'Rs.200',
+      LocationOfExpenditure : "Nashik",
+    }
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <h2 className='expense-item__description h2'>Expense Items</h2>
+      {expenses.map(expense => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          LocationOfExpenditure={expense.LocationOfExpenditure}
+        />
+      ))}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,33 +1,17 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
-  const LocationOfExpenditure = "Nashik";
   const expenseDate = new Date(2024,2,28);
-  const expenseTitle = 'Food';
-  const expenseAmount = 'Rs.10';
-
+ 
     return (
       <div>
-        <h2 className='expense-item__description h2'>Expense Items</h2>
         <div>
             <div className='expense-item'>
                 <div>{expenseDate.toISOString()}</div>
-                <h2 className='expense-item__description'>{LocationOfExpenditure}</h2>
-                <h2 className='expense-item__description'>{expenseTitle}</h2>
-                <div className='expense-item__price'>{expenseAmount}</div>
-            </div>
-            <div className='expense-item'>
-                <div>{expenseDate.toISOString()}</div>
-                <h2 className='expense-item__description'>{LocationOfExpenditure}</h2>
-                <h2 className='expense-item__description'>Petrol</h2>
-                <div className='expense-item__price'>Rs.100</div>
-            </div>
-            <div className='expense-item'>
-                <div>{expenseDate.toISOString()}</div>
-                <h2 className='expense-item__description'>{LocationOfExpenditure}</h2>
-                <h2 className='expense-item__description'>Movies</h2>
-                <div className='expense-item__price'>Rs.200</div>
+                <h2 className='expense-item__description'>{props.LocationOfExpenditure}</h2>
+                <h2 className='expense-item__description'>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
       </div>
