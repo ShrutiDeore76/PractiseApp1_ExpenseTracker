8be171e8e24b3ecd1b405041f45c917ff4d247a2
ShrutiDeore76 <shrutideore76@gmail.com>
2024-03-11 17:02:08 +0530

    Adding updated files ExpenseDetails.js And ExpenseItems.js : for splitting components practise

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,17 @@
+import ExpenseDate from './ExpenseDate';
+
+import ExpenseDetails from './ExpenseDetails';
+
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
-  const expenseDate = new Date(2024,2,28);
- 
     return (
-      <div>
-        <div>
             <div className='expense-item'>
-                <div>{expenseDate.toISOString()}</div>
-                <h2 className='expense-item__description'>{props.LocationOfExpenditure}</h2>
-                <h2 className='expense-item__description'>{props.title}</h2>
-                <div className='expense-item__price'>{props.amount}</div>
+            <ExpenseDate date={props.date} />
+            <ExpenseDetails amount={props.amount}  LocationOfExpenditure={props.LocationOfExpenditure}   title={props.title} />
             </div>
-        </div>
-      </div>
     );
   }
   
-  export default ExpenseItem;
+  export default ExpenseItem;  
