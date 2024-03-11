b43e2f10e9597be4c86328e0131067981914bc9d
ShrutiDeore76 <shrutideore76@gmail.com>
2024-03-11 12:02:44 +0530

    Adding Js Variables outputting Dynamic data practise

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,21 +1,35 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+
+  const LocationOfExpenditure = "Nashik";
+  const expenseDate = new Date(2024,2,28);
+  const expenseTitle = 'Food';
+  const expenseAmount = 'Rs.10';
+
     return (
       <div>
-        <h2>Expense Items</h2>
-        <table>
-            <tr>
-                <td>Food</td>
-                <td>Rs.10</td>
-            </tr>
-            <tr>
-                <td>Petrol</td>
-                <td>Rs.100</td>
-            </tr>
-            <tr>
-                <td>Movies</td>
-                <td>Rs.200</td>
-            </tr>
-        </table>
+        <h2 className='expense-item__description h2'>Expense Items</h2>
+        <div>
+            <div className='expense-item'>
+                <div>{expenseDate.toISOString()}</div>
+                <h2 className='expense-item__description'>{LocationOfExpenditure}</h2>
+                <h2 className='expense-item__description'>{expenseTitle}</h2>
+                <div className='expense-item__price'>{expenseAmount}</div>
+            </div>
+            <div className='expense-item'>
+                <div>{expenseDate.toISOString()}</div>
+                <h2 className='expense-item__description'>{LocationOfExpenditure}</h2>
+                <h2 className='expense-item__description'>Petrol</h2>
+                <div className='expense-item__price'>Rs.100</div>
+            </div>
+            <div className='expense-item'>
+                <div>{expenseDate.toISOString()}</div>
+                <h2 className='expense-item__description'>{LocationOfExpenditure}</h2>
+                <h2 className='expense-item__description'>Movies</h2>
+                <div className='expense-item__price'>Rs.200</div>
+            </div>
+        </div>
       </div>
     );
   }
