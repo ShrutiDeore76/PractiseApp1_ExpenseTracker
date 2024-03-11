b12ee98a4d8777a343ee56a8106e2f7e3a22abfb
ShrutiDeore76 <shrutideore76@gmail.com>
2024-03-11 17:18:04 +0530

    Adding App.js file with few changes

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ function App() {
       id: 'e1',
       title: 'Food',
       amount: 'Rs.10',
+      date: new Date(2020, 7, 14),
       LocationOfExpenditure : "Nashik",
     },
     {
       id: 'e2',
       title: 'Petrol',
       amount: 'Rs.100',
+      date: new Date(2021, 2, 28),
       LocationOfExpenditure : "Nashik",
     },
     {
       id: 'e3',
       title: 'Movies',
       amount: 'Rs.200',
+      date: new Date(2021, 5, 12),
       LocationOfExpenditure : "Nashik",
     }
   ];
@@ -30,6 +33,7 @@ function App() {
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
+          date={expense.date}
           LocationOfExpenditure={expense.LocationOfExpenditure}
         />
       ))}
