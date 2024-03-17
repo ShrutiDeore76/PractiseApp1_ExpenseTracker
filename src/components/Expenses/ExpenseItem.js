bbf1cee6f5a502711a4829d368bda1bd40ba2354
ShrutiDeore76 <shrutideore76@gmail.com>
2024-03-17 12:14:15 +0530

    Adding one file ExpenseItem,js with changes containing click and delete event handlers

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,10 +8,25 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 
+
+        const clickHandler = () => {
+                console.log("clicked !!!");
+              };
+
+        const deleteHandler = (event) =>{
+
+                const expenseToDelt = event.target.parentElement;
+                expenseToDelt.remove();
+                console.log("Deleted !!");
+
+        };
+
     return (
             <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <ExpenseDetails amount={props.amount}  LocationOfExpenditure={props.LocationOfExpenditure}   title={props.title} />
+            <button onClick={clickHandler}>Change Title</button>
+            <button onClick={deleteHandler}>Delete Expense</button>
             </Card>
     );
   }
